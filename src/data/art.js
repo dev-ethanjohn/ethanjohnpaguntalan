01ccf47cdb82986d610497f95aa76b9e5e3848f2
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-06-22 01:00:38 +0800

    feat: populate arts tab with art items

--- src/data/art.js
+++ src/data/art.js
@@ -1,17 +1,63 @@
-import artImage2 from "../images/2-art.jpg";
-import artImage3 from "../images/3-art.jpg";
+import artImage1 from "../images/art/1-art.jpeg";
+import artImage10 from "../images/art/10-art.png";
+import artImage11 from "../images/art/11-art.jpg";
+import artImage12 from "../images/art/12-art.jpg";
+import artImage2 from "../images/art/2-art.jpg";
+import artImage3 from "../images/art/3-art.jpg";
+import artImage4 from "../images/art/4-art.jpg";
+import artImage5 from "../images/art/5-art.jpg";
+import artImage6 from "../images/art/6-art.jpg";
+import artImage7 from "../images/art/7-art.jpg";
+import artImage8 from "../images/art/8-art.jpg";
+import artImage9 from "../images/art/9-art.jpg";
 
 export const arts = [
   {
-    id: "image-1",
-    src: "/images/project-1.jpg",
+    id: "oc-close-up",
+    src: artImage1.src,
   },
   {
-    id: "image-2",
+    id: "yuji-beach",
     src: artImage2.src,
   },
   {
-    id: "image-3",
+    id: "uchiha-eye",
     src: artImage3.src,
+  },
+  {
+    id: "loid-forger",
+    src: artImage4.src,
+  },
+  {
+    id: "tanjiro",
+    src: artImage5.src,
+  },
+  {
+    id: "spring-eye",
+    src: artImage6.src,
+  },
+  {
+    id: "oc-flower",
+    src: artImage7.src,
+  },
+  {
+    id: "shoto-todoroki",
+    src: artImage8.src,
+  },
+  {
+    id: "original-comic",
+    src: artImage9.src,
+  },
+  {
+    id: "me-pixel",
+    src: artImage10.src,
+  },
+  {
+    id: "dabi",
+    src: artImage11.src,
+  },
+  {
+    id: "stary-mountains",
+    src: artImage12.src,
   },
 ];
