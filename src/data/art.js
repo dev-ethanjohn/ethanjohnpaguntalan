e72943e5e6a6f86ef5d12305b574bc83faf01b56
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-06-21 20:50:54 +0800

    fix: correct prop matching between tab and modal

---
+++ src/data/art.js
@@ -0,0 +1,17 @@
+import artImage2 from "../images/2-art.jpg";
+import artImage3 from "../images/3-art.jpg";
+
+export const arts = [
+  {
+    id: "image-1",
+    src: "/images/project-1.jpg",
+  },
+  {
+    id: "image-2",
+    src: artImage2.src,
+  },
+  {
+    id: "image-3",
+    src: artImage3.src,
+  },
+];
