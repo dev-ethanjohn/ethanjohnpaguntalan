8d626e16e2a6123f94ddb3478e1a8605f7eb7de5
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-06-30 19:24:01 +0800

    chore: add project images and improve project image styling

--- src/data/project.js
+++ src/data/project.js
@@ -22,7 +22,7 @@ export const projects = [
     description: [
       "My portfolio is a thoughtfully crafted web showcase built with Astro for a static site generation framework, leveraging vanilla HTML, CSS, and JavaScript for a lightweight and customizable structure, View Transitions for smooth page animations, and Markdown for dynamic content management. Designed with a responsive and adaptive style using CSS, it adapts seamlessly across devices, from desktops to mobiles, ensuring an optimal viewing experience. This project highlights a blend of modern web development practices, emphasizing good adaptive design, while serving as a professional platform to display my projects, skills, and creative works. p.s. I would probably transition this project to React with React Router to handle even better client side interactions and transitions. Also, I will add more robust routing especially on desktop tabs",
     ],
-    src: "/images/project-1.jpg",
+    src: "/images/portfolio.png",
     category: "projects",
     stack: [
       TechStack.HTML,
@@ -56,7 +56,7 @@ export const projects = [
     description: [
       "Foodie AI is a collaborative web application developed with a friend, utilizing React for a dynamic frontend, React Router with the newer createBrowserRouter for efficient navigation, the Gemini API for advanced photo analysis, and Framer Motion for engaging animations. This innovative tool allows users to upload a meal photo, which Gemini analyzes to provide detailed insights including benefits, outcomes, ingredients, and nutrients. My contributions included organizing and refining styling, refactoring CSS to resolve responsive layout issues, enhancing animations and user interactions, simplifying JSX by reducing div containers for a more intuitive structure, converting global CSS to modular CSS files with a maintainable filing system for easier access and scalability, and refactoring routing to leverage modern createBrowserRouter, making Foodie AI a user-friendly and efficiently structured resource for food enthusiasts.",
     ],
-    src: "/images/project-2.jpg",
+    src: "/images/foodieAi.png",
     category: "projects",
     stack: [
       TechStack.REACT,
@@ -74,7 +74,7 @@ export const projects = [
     description: [
       "The Tip Calculator is a fun little project I tackled from a Frontend Mentor challenge, where I built a handy tool to figure out tips based on your bill and how good the service was. I used HTML to set up the structure, SCSS/SASS to style it with a clean and flexible look, ES6 modules to keep my JavaScript nice and organized, and plain JavaScript to make it all work smoothly with real-time feedback for users. I really enjoyed crafting a responsive design that’s easy to use, and it turned out to be a solid way to show off my frontend skills while meeting the challenge’s goals!",
     ],
-    src: "/images/project-1.jpg",
+    src: "/images/tip-calculator.png",
     category: "projects",
     stack: [
       TechStack.HTML,
@@ -92,7 +92,7 @@ export const projects = [
     description: [
       "The Meet Landing Page is a project I built from the ground up (Frontend Mentor), using Semantic HTML5 markup to create a well-structured and accessible foundation, CSS custom properties for easy theme management, Grid for dynamic layouts, Flexbox for flexible component alignment, and Media Queries to ensure it looks great on all screen sizes. Through this project, I dove deeper into mastering CSS Grid and Flexbox for adaptive layouts, got the hang of utility class flows, and honed my skills with media queries and positioning—especially for larger desktop screens",
     ],
-    src: "/images/project-1.jpg",
+    src: "/images/meet.png",
     category: "projects",
     stack: [TechStack.HTML, TechStack.CSS, TechStack.JAVASCRIPT],
   },
