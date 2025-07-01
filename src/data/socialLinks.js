ff179d96f994ee5286783d3118101c44d9de1a1f
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-07-01 10:24:16 +0800

    feat: add footer

---
+++ src/data/socialLinks.js
@@ -0,0 +1,27 @@
+export const links = [
+  {
+    name: "LinkedIn",
+    logo: "linkedIn",
+    url: "https://www.linkedin.com/in/ethanjohnpaguntalan-io/",
+  },
+  {
+    name: "Instagram",
+    logo: "instagram",
+    url: "https://www.instagram.com/dev.ejohn/",
+  },
+  {
+    name: "Code Wars",
+    logo: "codewars",
+    url: "https://www.codewars.com/users/dev-ethanjohn",
+  },
+  {
+    name: "Facebook",
+    logo: "facebook",
+    url: "https://www.facebook.com/ethanjohn.paguntalan/",
+  },
+  {
+    name: "Frontend Mentor",
+    logo: "frontendmentor",
+    url: "https://www.frontendmentor.io/profile/dev-ethanjohn",
+  },
+];
